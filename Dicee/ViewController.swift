7f975e9fd26923c46c0709ef4f3b684a9a17868e
AndreiFerradesign <ferradesign@yandex.ru>
2023-02-21 13:34:39 +0100

    Dicee application on UIKit

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var diceImageViewOne: UIImageView!
+    
+    @IBOutlet var diceImageViewTwo: UIImageView!
+    
+    @IBOutlet var rollButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+        diceImageViewOne.image = UIImage(named: "dice1")
+        diceImageViewTwo.image = UIImage(named: "dice1")
+        
+    }
+    @IBAction func rollButton(_ sender: Any) {
+        var n: Int
+        var m: Int
+        n = Int.random(in: 1...6)
+        m = Int.random(in: 1...6)
+        diceImageViewOne.image = UIImage(named: "dice\(n)")
+        diceImageViewTwo.image = UIImage(named: "dice\(m)")
+    }
+    
 
 }
 
